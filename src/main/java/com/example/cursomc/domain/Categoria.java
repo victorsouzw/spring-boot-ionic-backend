fa9eb48631d3fe371cebc9d33c2032e9029c8403
Victor Souza <svictorl@hotmail.com>
2022-01-19 15:10:19 -0300

    Resolvendo Categoria..

--- src/main/java/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -1,12 +1,20 @@
 package com.example.cursomc.domain;
-//pra fazer o dominio tem um checklist no material (passo a passo:)!!
+
 
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
@@ -38,7 +46,10 @@ public class Categoria implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -50,8 +61,15 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
+
 	
+
 	
 }
