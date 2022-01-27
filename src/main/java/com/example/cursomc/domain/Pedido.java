26293b66a719506b04fe8b4f8c6fb7291433ec96
Victor Souza <svictorl@hotmail.com>
2022-01-27 15:54:46 -0300

    subtotal e total do pedido

--- src/main/java/com/example/cursomc/domain/Pedido.java
+++ src/main/java/com/example/cursomc/domain/Pedido.java
@@ -60,6 +60,14 @@ public class Pedido implements Serializable{
 		
 	}
 	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
